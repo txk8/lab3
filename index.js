5a8a0013fd9922fa21df3cc027b86832a9b7076e
Kate Fowler <kitcat1570@googlemail.com>
2021-02-26 11:03:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var http = require("http");
+
+const PORT = process.env.PORT || 8080;
+
 http.createServer(function (request, response) {
- // Send the HTTP header
- // HTTP Status: 200 : OK
- // Content Type: text/plain
- response.writeHead(200, {'Content-Type': 'text/plain'});
- // Send the response body as "Hello World"
- response.end('Hello World\n');
- const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
- console.log(`Our app is running on port ${3000}`);
-});
-});
-//.listen(PORT);
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(PORT);
+
 // Console will print the message
-//console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:' + PORT);
